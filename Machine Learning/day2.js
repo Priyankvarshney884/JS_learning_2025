08948f6e60c41722d4e8ad4e10a27eb23fb545c1
Priyank Varshney <Priyank.varshney@electrolux.com>
2025-07-02 10:30:50 +0530

    machinecoding changes

---
+++ Machine Learning/day2.js
@@ -0,0 +1,14 @@
+/* topics which we will learn today 
+Fragments
+Event handling
+Mouse and Keyboard events
+Event Bubbling and Capturing
+Event Delegation
+Event Listeners
+Event Object
+Event Propagation
+Event Target
+Event Types
+
+*/
+
